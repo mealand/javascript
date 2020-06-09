b50f8eb99d6f454bd80bd7ac896628b05bff9a1d
emm <followmenot@live.com>
2020-06-09 10:30:04 +0100

    First Clock Changes

---
+++ clock/clock.js
@@ -0,0 +1,14 @@
+function getTime(){
+
+    let timing = document.getElementById("selected-time").value;
+    let timed = parseFloat(timing);
+    let sleeptime = timed-4;
+    let waketime = timing;
+    let naptime = timed+8;
+
+    document.getElementById("sleep-time").value = "Sleep by "+sleeptime+":00 hours";
+    document.getElementById("wake-time").value = "Wake up by "+waketime+":00 hours";
+    document.getElementById("nap-time").value = "Nap for 1 hour starting from "+naptime+":00 hours";
+
+ 
+}
