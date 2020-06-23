80854f1bd9acf1b12ce99fe85b9fe2bdd960d20f
emm <followmenot@live.com>
2020-06-23 21:27:49 +0100

    Time changes

--- clock/clock.js
+++ clock/clock.js
@@ -6,9 +6,22 @@ function getTime(){
     let waketime = timing;
     let naptime = timed+8;
 
+
+
     document.getElementById("sleep-time").value = "Sleep by "+sleeptime+":00 hours";
     document.getElementById("wake-time").value = "Wake up by "+waketime+":00 hours";
     document.getElementById("nap-time").value = "Nap for 1 hour starting from "+naptime+":00 hours";
+    
 
- 
+    
+}
+function presentTime(){
+    let x = 1;
+    let newtime = new Date();
+    let currentTime = newtime.toLocaleTimeString();
+    
+    document.getElementById("current-time").value = currentTime;
+    setTimeout("presentTime()",1000);
+    
+
 }
