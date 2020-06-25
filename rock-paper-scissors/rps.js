1ed93c9577213b79e5ca5e92a56c1dc38c0cca61
emm <followmenot@live.com>
2020-06-25 22:07:51 +0100

    javascript changes

--- rock-paper-scissors/rps.js
+++ rock-paper-scissors/rps.js
@@ -1,8 +1,20 @@
+let player_rock = document.getElementById("rock-button").value;
+let player_paper = document.getElementById("paper-button").value;
+let player_scissors = document.getElementById("scissors-button").value;
+
+let compt_rock;
+let compt_paper;
+let compt_scisoors;
+
+let range = Math.floor(Math.random() * 3);
+let compt_choice;
+alert(range);
+
 function introduction(){
-    document.getElementById("choose-img").src = "http://sprezzint.com/wp-content/uploads/2020/06/rock2.jpg";
 
 }
 function playerschoice(){
+    document.getElementById("choose-img").src = ""
 
 }
 function computerschoice(){
